ec835ee24f1ee3f4d571adcdd02799923d8954be
saminazahra123 <saminazahra4422@gmail.com>
2025-11-09 12:28:27 -0800

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = array.from(buttons);
+arr.foreach(button =>{
+    button.addEventListener('click', (e) =>{
+        if (e.target.innerHTML == "= "){
+            string = eval(string);
+            input.value = string;
+        }
+        string += e.target.innerHTML;
+        input.value = string ;
+    })
+})
